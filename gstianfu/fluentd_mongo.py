fc238225c633d4669f4f1575234db5eed44c4a93
007root <007root@users.noreply.github.com>
2017-12-17 08:08:45 +0800

    fix bug
    if not data record last time

--- gstianfu/fluentd_mongo.py
+++ gstianfu/fluentd_mongo.py
@@ -1,35 +1,36 @@
 #!/usr/bin/env python
-
 from fluent import sender
 import os
 import logging
 
+
+time_file = "/tmp/.fluentd_timestamp"
 logger = logging.getLogger('fluentd')
 logger.setLevel(logging.INFO)
-
 fh = logging.FileHandler('/tmp/fluentd.log')
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 fh.setFormatter(formatter)
 logger.addHandler(fh)
-
 fl = sender.FluentSender('mongodb', '192.168.4.57', 8888)
-
 log_path = "./mongod.log"
 timestamp=None
+
+
 try:
-    f = open('./.fluentd_timestamp')
+    f = open(time_file)
     timestamp = f.read()
     f.close()
 except IOError:
-    f = open('./.fluentd_timestamp','w')
+    f = open(time_file,'w')
     f.close()
 
+    
 # query time >= 300ms
 if timestamp:
     logger.info('method grep')
-    logs = os.popen("grep -a -v 'aggregate' %s \
-                 | grep -a -A 100 '%s' | sed -n '2,$p'\
-                 | egrep '[3-9][0-9]{2}ms|[0-9]{4,}ms'"% (log_path, timestamp)).read()
+    logs = os.popen("grep -a -A 3000 '%s' %s | grep 'COMMAND'\
+                 |grep -a -v 'aggregate' | sed -n '2,$p'\
+                 | egrep '[3-9][0-9]{2}ms|[0-9]{4,}ms'"% (timestamp, log_path)).read()
 else:
     logger.info('method tail')
     logs = os.popen("grep -a -v 'aggregate' %s \
@@ -37,7 +38,7 @@ else:
 
     
 def update_time(stamp):
-    f = open('./.fluentd_timestamp', 'w')
+    f = open(time_file, 'w')
     f.write(stamp)
     f.close()
 
@@ -54,4 +55,7 @@ if logs:
             logger.info('Last date "%s"' % up_time)
 else:
     logger.info('Not data')
-    
+    up_time = os.popen("grep -a -A 3000 '%s' %s | grep 'COMMAND'\
+                       | grep -a -v 'aggregate' | tail -1" % (timestamp, log_path)).read()
+    update_time(up_time)
+    logger.info('Update timestamp to %s' % up_time
