661ec05e79a42c91efc37a9ddd32ee4ec1d6ecd5
007root <007root@users.noreply.github.com>
2018-06-21 16:45:30 +0800

    Create export_mongo_to_mysql.py

---
+++ gstianfu/export_mongo_to_mysql.py
@@ -0,0 +1,48 @@
+from pymongo import MongoClient, ASCENDING
+from bson.objectid import ObjectId
+from datetime import datetime
+import MySQLdb
+
+
+HOST = "192.168.4.5"
+USER = "root"
+PASS = "root"
+KEY = "INSERT INTO `django_session` VALUES "
+VALUE = "(\"%s\", \"%s\", \"%s\")"
+v_list = []
+
+mongo_client = MongoClient(host=HOST)
+mongo_db = mongo_client.test
+session = mongo_db.session
+user_info = mongo_db.user_info
+
+mysql_db = MySQLdb.connect(HOST, USER, PASS, "test")
+cursor = mysql_db.cursor()
+
+
+def get_user_id(uid=None):
+    if uid == None:
+        ret = user_info.find({}, {"user_id": 1}).sort("user_id", ASCENDING).limit(500)
+    else:
+        ret = user_info.find({"user_id": {"$gt": uid}}, {"user_id": 1}).sort("user_id", ASCENDING).limit(500)
+    return ret
+
+
+result = get_user_id()
+while True:
+    for user in result:
+        if user.get("user_id", None):
+            ret = session.find({"$and": [{"user_id": user["user_id"]}, {"expire_date": {"$gt": datetime.today()}}]})
+            if ret.count() != 0:
+                for r in ret:
+                    v_list.append(VALUE % (r["session_key"], r["session_data"], r["expire_date"]))
+                values = ','.join(v_list)
+                exec_sql = KEY + values + ";"
+                cursor.execute(exec_sql)
+                mysql_db.commit()
+                v_list = []
+    else:
+        result = get_user_id(user["user_id"])
+    if result.count() == 0:
+        break
+
