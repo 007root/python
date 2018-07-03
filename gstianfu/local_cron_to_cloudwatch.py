9eda0fafb459f1f070d1b517fccf71b6e99cbe24
007root <007root@users.noreply.github.com>
2018-07-03 18:00:48 +0800

    fix bug

--- gstianfu/local_cron_to_cloudwatch.py
+++ gstianfu/local_cron_to_cloudwatch.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python
+import re
+from exceptions import Exception
 
 ret = ""
 c_list = ["minute",
@@ -23,16 +25,34 @@ def hour_deal(arg):
 
 def hours_to_utc(arg):
     ret = []
+    tmp = []
     for h in arg:
-        _, utc_h = hour_deal(h)
-        ret.append(utc_h)
+        _, utc_h = hour_deal(h) 
+        tmp.append(str(utc_h))
+    if len(arg) >= 2:
+        delimiter = re.search(',|-', bj_hour).group()
+        ret.append(delimiter.join(tmp))
+    else:
+        ret = tmp
     return ret
-    
-    
-def to_utc(hour, day, week):
+
+
+def day_to_utc(arg):
+    ret = []
+    tmp = []
+    if len(arg) >= 2:
+        tmp = map(lambda x: str(int(x) - 1) if int(x) != 1 else "L", arg)
+        delimiter = re.search(',|-', bj_day).group()
+        ret.append(delimiter.join(tmp))
+    else:
+        ret = map(lambda x: str(int(x) - 1) if int(x) != 1 else "L", arg)
+    return ret
+        
+
+def to_utc(h, d, week):
     ret = {"minute": [bj_minute], "month": [bj_month], "year": ["*"]}
-    hour = hour.split(',')
-    day = day.split(',')
+    hour = re.split(',|-', h)
+    day = re.split(',|-', d)
     if hour[0] == "*" or "/" in hour[0]:
         ret["hour"] = hour
     if day[0] == "*" or "/" in day[0]:
@@ -59,28 +79,35 @@ def to_utc(hour, day, week):
                     ret["day"] = [int(day[0]) - 1]
                 ret["hour"] = [utc_h]
             else:
-                ret["day"] = day
+                ret["day"] = [bj_day]
                 ret["hour"] = [utc_h]
         elif hour_num > 1 and day_num >= 1:
             hour = [int(i) for i in hour]
             other_day = filter(lambda x: x < 8, hour)
             today = filter(lambda x: x > 8, hour)
             if len(other_day) == hour_num:
-                ret["day"] = map(lambda x: int(x) - 1 if int(x) != 1 else "L", day)
+                ret["day"] = day_to_utc(day)
             elif len(today) == hour_num:
-                ret["day"] = day
+                ret["day"] = [bj_day]
             if ret.get("day"):
                 utc_h = hours_to_utc(hour)
                 ret["hour"] = utc_h
+        elif hour_num == 1:
+            is_yesterday, utc_h = hour_deal(hour[0])
+            if is_yesterday:
+                ret["day"] = day_to_utc(day)
+            else:
+                ret["day"] = [bj_day]
+            ret['hour'] = [utc_h]
         ret["week"] = ["?"]
     elif ret_day and not ret_hour:
         utc_h = hours_to_utc(hour)
         ret["hour"] = utc_h
     else:
         ret["day"] = day
-    return ret
-    
-    
+    return ret     
+
+
 if __name__ == "__main__":
     cron = raw_input("crontab: ")
     timing = cron.split(' ')
@@ -91,8 +118,10 @@ if __name__ == "__main__":
     bj_week = timing[4]
     result=to_utc(bj_hour, bj_day, bj_week)
     for field in c_list:
-        for i in result[field]:
-            i = str(i) + ' '
-            ret += i
+        if result.get(field):
+            for i in result[field]:
+                i = str(i) + ' '
+                ret += i
+        else:
+            raise Exception('FormatError')
     print(ret)
-
