9ff35021ff4d392d40cf0d49fcc1bcfd1f189737
007root <007root@users.noreply.github.com>
2018-07-04 09:47:50 +0800

    fix bug

--- gstianfu/local_cron_to_cloudwatch.py
+++ gstianfu/local_cron_to_cloudwatch.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import re
 from exceptions import Exception
+import argparse
 
 ret = ""
 c_list = ["minute",
@@ -39,32 +40,41 @@ def hours_to_utc(arg):
 
 def day_to_utc(arg):
     ret = []
-    tmp = []
+    tmp = map(lambda x: str(int(x) - 1) if int(x) != 1 else "L", arg)
     if len(arg) >= 2:
-        tmp = map(lambda x: str(int(x) - 1) if int(x) != 1 else "L", arg)
         delimiter = re.search(',|-', bj_day).group()
         ret.append(delimiter.join(tmp))
     else:
-        ret = map(lambda x: str(int(x) - 1) if int(x) != 1 else "L", arg)
+        ret = tmp
     return ret
-        
 
-def to_utc(h, d, week):
+
+def week_to_utc(week):
+    ret = []
+    t = re.split(',|-', week)
+    if t[0] == "*":
+        ret = ["?"]
+    else:
+        tmp = map(lambda x: str(int(x) + 1) if int(x) < 7 else "1", t)
+        if len(t) >= 2:
+            delimiter = re.search(',|-', week).group()
+            ret.append(delimiter.join(tmp))
+        else:
+            ret = tmp
+    return ret
+       
+
+def to_utc(hour, day, week):
     ret = {"minute": [bj_minute], "month": [bj_month], "year": ["*"]}
-    hour = re.split(',|-', h)
-    day = re.split(',|-', d)
+    hour = re.split(',|-', hour)
+    day = re.split(',|-', day)
     if hour[0] == "*" or "/" in hour[0]:
         ret["hour"] = hour
     if day[0] == "*" or "/" in day[0]:
         ret["day"] = day
-        ret["week"] = ["?"]
-    if week[0] != "*":
-        if int(week[0]) >= 7:
-            ret["week"] = [1]
-        else:
-            ret["week"] = [int(week) + 1]
+    ret["week"] = week_to_utc(week)
+    if "*" not in ret['week'] and "?" not in ret['week']:
         ret["day"] = ["?"]
-
     hour_num = len(hour)
     day_num = len(day)
     ret_day = ret.get("day")
@@ -108,8 +118,19 @@ def to_utc(h, d, week):
     return ret     
 
 
+def get_parse():
+    arg = argparse.ArgumentParser()
+    arg.add_argument("-c", "--convert", default="", 
+                        help='Convert linux crontab eg("3 4-7 4 * *")\
+                         to AWS CloudWatch ("3 20-23 3 * ? *")')
+    return arg.parse_args()
+
+
 if __name__ == "__main__":
-    cron = raw_input("crontab: ")
+    args = vars(get_parse())
+    cron = args["convert"]
+    if len(cron.split(' ')) != 5:
+        raise Exception('Crontab format error')
     timing = cron.split(' ')
     bj_minute = timing[0]
     bj_hour = timing[1]
@@ -117,6 +138,7 @@ if __name__ == "__main__":
     bj_month = timing[3]
     bj_week = timing[4]
     result=to_utc(bj_hour, bj_day, bj_week)
+    print("Linux: %s" % cron)
     for field in c_list:
         if result.get(field):
             for i in result[field]:
@@ -124,4 +146,5 @@ if __name__ == "__main__":
                 ret += i
         else:
             raise Exception('FormatError')
-    print(ret)
+    print("CloudWatch: %s" % ret)
+
