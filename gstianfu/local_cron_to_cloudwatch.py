53b7fdf0a4ae07a7109b020aa43e4dd8afd87e79
007root <007root@users.noreply.github.com>
2018-07-04 14:00:31 +0800

    add description

--- gstianfu/local_cron_to_cloudwatch.py
+++ gstianfu/local_cron_to_cloudwatch.py
@@ -119,10 +119,9 @@ def to_utc(hour, day, week):
 
 
 def get_parse():
-    arg = argparse.ArgumentParser()
+    arg = argparse.ArgumentParser(description='Example:\n\tto_cloudwatch -c "2 3,3 4 * 4"', formatter_class=RawTextHelpFormatter)
     arg.add_argument("-c", "--convert", default="", required=True, 
-                        help='Convert linux crontab eg("3 4-7 4 * *")\
-                         to AWS CloudWatch ("3 20-23 3 * ? *")')
+                      help='Convert linux crontab eg("3 4-7 4 * *") \nto AWS CloudWatch ("3 20-23 3 * ? *")')
     return arg.parse_args()
 
 
